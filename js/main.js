871e59610b75cc20b298fb18f5d78f1b521301aa
Garrett Hong <choy66woy@gmail.com>
2018-05-03 16:34:40 -0700

    into

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(1440, 760, Phaser.AUTO, '');
 
 game.state.add("running", running(game));
-game.state.add("intro");
+game.state.add("intro", intro(game));
 //game.states.add("gameover", gameover(game));
 
-game.state.start("running");
+game.state.start("intro");
--- js/states/intro.js
+++ js/states/intro.js
@@ -1,33 +1,31 @@
-function running(game) {
-    const menuState = {
-
-      /**
-       * Loads game assets (images, sounds, tilemaps, etc)
-       */
-      preload : function () {
-           game.load.spritesheet('crash', 'assets/crashclone.png', 132, 147);
-      },
-
-      /**
-       * Initializes variables and instantiates objects
-       */
-      create: function () {
+function intro(game) {
+    console.warn("intro");
+    
+    function preload(){
+        game.load.spritesheet('zom', 'assets/.png', 132, 147);
+    }
+    
+    function create(){
         game.add.text(
           80, 260,  // x, y position
           "Welcome to the menu state. Click to begin!", 
           { fontSize: "32px", fill: "#fff" }
         );
-      },
-
-      /** 
-       * Updates the screen each frame
-       */
-      update: function () {
-
-
+    }
+    
+    function update(){
         if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
           game.state.start("running");
         }
-      }
-    };
+    }
+    
+    function render(){
+    }
+    
+    return {
+        preload,
+        create,
+        update,
+        render
+    }
 }
