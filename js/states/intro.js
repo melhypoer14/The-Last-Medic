78b71c8a9a07250184a0e1d3de334fe90502d905
Garrett Hong <choy66woy@gmail.com>
2018-05-08 17:41:53 -0700

    deleted stuff fixed intro

--- js/states/intro.js
+++ js/states/intro.js
@@ -8,7 +8,7 @@ function intro(game) {
     }
     
     function create(){
-        var melee = game.add.sprite(0, -200, 'exp');
+        var melee = game.add.sprite(0, -140, 'exp');
         var walk = melee.animations.add('exp');
         melee.animations.add('exp', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47], 12, true);
         melee.animations.play('exp');
